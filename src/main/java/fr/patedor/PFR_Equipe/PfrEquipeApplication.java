d6e200d999b500ae1ac861d8c47c2c494dc23abc
Clara <clara.laviale@gmail.com>
2025-03-04 16:36:26 +0100

    initial commit

---
+++ src/main/java/fr/patedor/PFR_Equipe/PfrEquipeApplication.java
@@ -0,0 +1,13 @@
+package fr.patedor.PFR_Equipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfrEquipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfrEquipeApplication.class, args);
+	}
+
+}
